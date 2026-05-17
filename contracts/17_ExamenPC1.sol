277cafac14258b4a55857e19dc41e29656c30fb5
Jaredsito28 <carlosjaredlescano28@gmail.com>
2026-05-16 20:24:29 -0500

    Parte 3

--- contracts/17_ExamenPC1.sol
+++ contracts/17_ExamenPC1.sol
@@ -5,26 +5,35 @@ import "hardhat/console.sol";
 
 contract Universidad000254786 {
 
+    modifier mostrarEjecucion() {
+    console.log("Ejecutado por: 000254786 - Juan Carlos Jared Lescano Leon");
+    _;
+}
+
     struct Estudiante {
         uint256 id;
         string nombre;
         uint256 edad;
+        bool estado;
     }
 
     Estudiante[] public estudiantes;
-    constructor() {
-    console.log("Ejecutado por: 000254786 - Juan Carlos Jared Lescano Leon");
+constructor() mostrarEjecucion {
 }
 
 function agregarElemento(
     uint256 _id,
     string memory _nombre,
     uint256 _edad
-) public {
-    estudiantes.push(Estudiante(_id, _nombre, _edad));
+) public mostrarEjecucion {
+    for (uint256 i = 0; i < estudiantes.length; i++) {
+    require(estudiantes[i].id != _id, "No se puede registrar un ID repetido");
+}
+    require(bytes(_nombre).length > 0, "El nombre no puede estar vacio");
+    estudiantes.push(Estudiante(_id, _nombre, _edad, true));
 }
 
-function contarElementos() public view returns (uint256) {
+function contarElementos() public view mostrarEjecucion returns (uint256) {
     console.log("Ejecutado por: 000254786 - Juan Carlos Jared Lescano Leon");
     return estudiantes.length;
 }
