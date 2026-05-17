3229cab9d292b43603c3e8e5fe898ae0ddd600ee
Jaredsito28 <carlosjaredlescano28@gmail.com>
2026-05-16 19:44:02 -0500

    Parte 1

---
+++ contracts/17_ExamenPC1.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+import "hardhat/console.sol";
+
+contract Universidad000254786 {
+
+    struct Estudiante {
+        uint256 id;
+        string nombre;
+        uint256 edad;
+    }
+
+    Estudiante[] public estudiantes;
+    constructor() {
+    console.log("Ejecutado por: 000254786 - Juan Carlos Jared Lescano Leon");
+}
+
+}
